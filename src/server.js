b5532ae4f8910a260604711786e7dbe4472b8103
jimdcodes <jdinhcodes@gmail.com>
2023-07-06 22:30:50 -0700

    MongoDB fully integrated

--- src/server.js
+++ src/server.js
@@ -1,59 +1,74 @@
 import express from 'express';
+import { db, connectToDb } from './db.js';
 
-let articlesInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'learn-mongodb',
-    upvotes: 0,
-    comments: [],
-}]
+// let articlesInfo = [{
+//     name: 'learn-react',
+//     upvotes: 0,
+//     comments: [],
+// }, {
+//     name: 'learn-node',
+//     upvotes: 0,
+//     comments: [],
+// }, {
+//     name: 'learn-mongodb',
+//     upvotes: 0,
+//     comments: [],
+// }]
 
 //[{ name: 'learn-react', upvotes: 0, comments: [], }, { name: 'learn-node', upvotes: 0, comments: [], }, { name: 'learn-mongodb', upvotes: 0, comments: [], }]
 
 const app = express();
 app.use(express.json());
 
-// app.post('/hello', (req, res) => {
-//     res.send(`Hello ${req.body.name}!`);
-// });
+//Allowing us to read information from MongoDB
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
 
-// app.get('/hello/:name', (req, res) => {
-//     const { name } = req.params;
-//     res.send(`Hello ${name}!!`);
-// });
+    const article = await db.collection('articles').findOne({ name });
+
+    if(article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404).send('Poopoo');
+    }    
+});
 
 // Adding upvotes capability to the server
-app.put('/api/articles/:name/upvote', (req, res) => {
-    const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+app.put('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params; 
+
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
     if (article) {
-        article.upvotes += 1;
-        res.send(`The ${name} article has ${article.upvotes} upvotes!!!`);
+        res.send(`The ${name} article has ${article.upvotes} upvote(s)!`);
     } else {
         res.send('That article doesn\'t exist')
     }
 });
 
 // Adding comments to the server
-app.post('/api/articles/:name/comments', (req, res) =>{
+app.post('/api/articles/:name/comments', async (req, res) =>{
     const { name } = req.params;
     const { postedBy, text } = req.body;
-    const article = articlesInfo.find(a => a.name === name);
 
-    if (article){
-        article.comments.push({ postedBy, text });
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article){        
         res.send(article.comments);
     } else {
         res.send('That article doesn\'t exist!')
     }    
 });
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
-});
+connectToDb(() => {
+    console.log('Successfully connected to database!');
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
+})
